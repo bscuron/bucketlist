7c2a9fd7b510df151eec2461ff40603ec2ba9d75
Ben Scuron <bscuron19@gmail.com>
2023-03-21 19:00:00 -0400

    Create `Actionsheet` navigator for `DatabaseScreen`

--- src/screens/DatabaseScreen.tsx
+++ src/screens/DatabaseScreen.tsx
@@ -1,7 +1,18 @@
 import React, { memo, useState, useEffect, useContext } from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { Context } from '../../App';
-import { VStack, HStack, Heading, Text } from 'native-base';
+import {
+    View,
+    Box,
+    VStack,
+    HStack,
+    Heading,
+    Text,
+    Actionsheet,
+    Icon
+} from 'native-base';
 import axios from 'axios';
+import { Ionicons } from '@expo/vector-icons';
 
 type DBRow = {
     id: number;
@@ -14,7 +25,9 @@ type DBRow = {
 
 const DatabaseScreen = () => {
     const [data, setData] = useState<DBRow[]>([]);
+    const [showNavigation, setShowNavigation] = useState<boolean>(false);
     const { token, removeItem } = useContext(Context);
+    const navigation = useNavigation();
 
     // Runs on component mount
     useEffect(() => {
@@ -45,17 +58,59 @@ const DatabaseScreen = () => {
             });
     }, []);
 
+    useEffect(() => {
+        // Use `setOptions` to update the button that we previously specified
+        // Now the button includes an `onPress` handler to update the count
+        navigation.setOptions({
+            headerRight: () => (
+                <Icon
+                    as={Ionicons}
+                    name="menu"
+                    onPress={() => setShowNavigation(!showNavigation)}
+                    color="black"
+                    size="2xl"
+                    mx="3%"
+                />
+            )
+        });
+    }, [navigation]);
+
     return (
-        <VStack space={4}>
-            <HStack space={3}>
-                <Heading>ID</Heading>
-                <Heading>USERNAME</Heading>
-                <Heading>PASSWORD</Heading>
-                <Heading>SECRET</Heading>
-                <Heading>RDATETIME</Heading>
-            </HStack>
-            {data}
-        </VStack>
+        <View>
+            <VStack space={4}>
+                <HStack space={3}>
+                    <Heading>ID</Heading>
+                    <Heading>USERNAME</Heading>
+                    <Heading>PASSWORD</Heading>
+                    <Heading>SECRET</Heading>
+                    <Heading>RDATETIME</Heading>
+                </HStack>
+                {data}
+            </VStack>
+            <Actionsheet
+                isOpen={showNavigation}
+                onClose={() => setShowNavigation(false)}
+            >
+                <Actionsheet.Content>
+                    <Box
+                        w="100%"
+                        h={60}
+                        px={4}
+                        alignItems="center"
+                        justifyContent="center"
+                    >
+                        <Heading size="md">Bucketlist</Heading>
+                    </Box>
+                    <Actionsheet.Item>Home</Actionsheet.Item>
+                    <Actionsheet.Item isDisabled>Database</Actionsheet.Item>
+                    <Actionsheet.Item>Profile</Actionsheet.Item>
+                    <Actionsheet.Item>Map</Actionsheet.Item>
+                    <Actionsheet.Item onPress={() => setShowNavigation(false)}>
+                        Cancel
+                    </Actionsheet.Item>
+                </Actionsheet.Content>
+            </Actionsheet>
+        </View>
     );
 };
 
