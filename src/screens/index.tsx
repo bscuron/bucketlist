6b6b4a20b08f838774030669bfc295c051c9d0f5
Ben Scuron <bscuron19@gmail.com>
2023-03-21 17:56:47 -0400

    Create a `LoadingScreen`

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,3 +1,4 @@
 export { default as SignupScreen } from './SignupScreen';
 export { default as DatabaseScreen } from './DatabaseScreen';
 export { default as LoginScreen } from './LoginScreen';
+export { default as LoadingScreen } from './LoadingScreen';
