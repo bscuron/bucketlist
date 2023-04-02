3ab9a70f93f5beefbb00c8602335a4a1e714f2a8
Ben Scuron <bscuron19@gmail.com>
2023-04-01 21:01:20 -0400

    Print `dob` and `r_datetime` as formatted strings on `ProfileScreen`

--- src/types/Profile.ts
+++ src/types/Profile.ts
@@ -1,11 +1,12 @@
 type Profile = {
-    username: string,
+    username: string;
     first_name?: string;
     last_name?: string;
     gender?: string;
     dob?: string;
     introduction?: string;
     picture?: string;
+    r_datetime?: string;
 };
 
 export default Profile;
