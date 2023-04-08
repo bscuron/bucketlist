135e556d8e3e67c27129cffcb8d4f131f22c581b
CurtisLam <tun07683@temple.edu>
2023-04-08 14:30:19 -0400

    added footer into login view to show information such as about us

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -4,3 +4,6 @@ export { default as LoadingScreen } from './LoadingScreen';
 export { default as HomeScreen } from './HomeScreen';
 export { default as ProfileScreen } from './ProfileScreen';
 export { default as LogoutScreen } from './LogoutScreen';
+export { default as SponsorScreen } from './SponsorScreen';
+export { default as TermsScreen } from './TermsScreen';
+export {default as PrivacyPolicyScreen} from './PrivacyPolicyScreen'
