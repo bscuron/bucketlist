323de1d36a2e22691ad4b8abf3dd7e73fe71b5cb
CurtisLam <tun07683@temple.edu>
2023-04-11 22:22:41 -0400

    Altered footer position

--- src/components/FooterView.tsx
+++ src/components/FooterView.tsx
@@ -3,23 +3,32 @@ import { StyleSheet } from 'react-native';
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 
-const FooterView =() =>{
+const FooterView = () => {
     const navigation = useNavigation();
-    return(
-        <Stack direction='column' style={styles.stack}>
-            <HStack space='5'>
-                <Text>About Us</Text>
-                <Text onPress={() => navigation.navigate('Sponsor')}>Sponsor Us</Text>
+    return (
+        <Stack direction="column" style={styles.stack}>
+            <HStack space="5">
+                <Text onPress={() => navigation.navigate('About')}>
+                    About Us
+                </Text>
+                <Text onPress={() => navigation.navigate('Sponsor')}>
+                    Sponsor Us
+                </Text>
                 <Text onPress={() => navigation.navigate('Terms')}>Terms</Text>
-                <Text onPress={() => navigation.navigate('Privacy')}>Privacy Policy</Text>
+                <Text onPress={() => navigation.navigate('Privacy')}>
+                    Privacy Policy
+                </Text>
                 <Text>Contact Us</Text>
             </HStack>
-            <HStack alignItems='center' mt='5'>
-                <Text>@{new Date().getFullYear()} Temple University SP23 BucketList Project Team </Text>
+            <HStack alignItems="center" mt="5">
+                <Text>
+                    @{new Date().getFullYear()} Temple University SP23
+                    BucketList Project Team{' '}
+                </Text>
             </HStack>
         </Stack>
-    )
-}
+    );
+};
 
 export default FooterView;
 
