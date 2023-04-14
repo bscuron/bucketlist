8806e97f1868bc72ea8980deb296fa4286b74fc7
lkan928 <tui98592@temple.edu>
2023-04-13 22:25:24 -0400

    edited the project description on the about page

--- src/screens/AboutScreen.tsx
+++ src/screens/AboutScreen.tsx
@@ -25,11 +25,28 @@ const AboutScreen = () => {
                     <Heading size="3xl">Welcome to BucketList!</Heading>
                     <Text fontSize="16">
                         Check off and complete your wish list with friends, and
-                        keep all your memory without regrat.
+                        keep all your memory without regret.
+                    </Text>
+
+                    <Text fontSize="16">
+                        BucketList provides the ability to create an interactive list of activities 
+                        you may want to accomplish. With this list, you can connect with old and 
+                        new friends to complete these activities and live a more fulfilled life. 
+                        Many times, we want to do things but never do them. BucketList is 
+                        intended to get people out and to accomplish their goals. 
+                        With the help of social connection and common interests' users can 
+                        connect and live the life they want to live instead of daydreaming but never doing.
+                    </Text>
+                    <Text fontSize="16">
+                        Through social connection and event tracking and planning we can all reach our goals. 
+                        Not only will your upcoming events be available on your bucket list, you can also view
+                         your friends upcoming events too! 
+                        This gives people incentive to get out and try new things!
                     </Text>
                 </VStack>
-                <Spacer size="10" />
+                
             </VStack>
+            <Spacer size="10" />
             <Container position="fixed" bottom="0" alignSelf="center">
                 <FooterView />
             </Container>
