d8c1db58c527024cfa0abceecf37323cbea22c33
Dennis Liang <dennis@Denniss-MacBook-Pro.local>
2023-04-25 14:12:25 -0400

    confirmation message

--- src/screens/SponsorScreen.tsx
+++ src/screens/SponsorScreen.tsx
@@ -1,4 +1,5 @@
 import { useNavigation } from '@react-navigation/native';
+
 import axios from 'axios';
 import {
     AlertDialog,
@@ -39,6 +40,8 @@ const SponsorScreen = () => {
     const [showCode, setShowCode] = useState<boolean>(false);
     const [codeData, setCodeData] = useState<string>('');
     const [backupCode, setBackupCode] = useState<string>('');
+    const [showBox, setShowBox] = useState(true);
+    const [showConfirm, setShowConfirm] = useState(false);
     const navigation = useNavigation();
 
     /**
@@ -46,14 +49,22 @@ const SponsorScreen = () => {
      */
     const submit = async () => {
         if (!validate()) return;
-        //await insertSponsor();
+        await insertSponsor();
+    };
+
+    const back = async () => {
+        setShowBox(true);
+        setShowConfirm(false);
+        navigation.navigate('Login');
+
     };
 
     /**
      * Sponsor request
      */
     const insertSponsor = async () => {
-        
+
+
         try {
             const result = await axios.post(
                 'https://cis-linux2.temple.edu/bucketlistBackend/sponsor/create',
@@ -62,12 +73,12 @@ const SponsorScreen = () => {
                     email: data.email
                 }
             );
-            //setCodeData(result.data.qrcode);
-            //setBackupCode(result.data.backupcode);
+            setShowBox(false);
+            setShowConfirm(true);
+            setShowCode(true);
         } catch (error) {
             return;
         }
-        setShowCode(true);
     };
 
     /**
@@ -99,25 +110,10 @@ const SponsorScreen = () => {
             return false;
         }
 
-        // Check if username exists in database already
-        axios
-            .get(
-                `https://cis-linux2.temple.edu/bucketlistBackend/database/sponsors/email/${email}`
-            )
-            .then((res) => {
-                if (res.data.rows.length > 0) {
-                    setErrors({
-                        ...errors,
-                        email: 'Email is already being used by another account'
-                    });
-                    return false;
-                }
-            })
-            .catch((error) => {
-                throw error;
-            });
-
-        delete errors.email;
+        setErrors({
+            ...errors,
+            email: ''
+        });
         return true;
     };
 
@@ -126,58 +122,71 @@ const SponsorScreen = () => {
             <KeyboardAvoidingView
                 behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
             >
-                <Center>
-                    <Box safeArea p="2" w="90%" maxW="290" py="8">
-                        <Heading size="xl">Sponsor us today!</Heading>
-                        <Heading mt="1" size="sm" fontWeight="normal">
-                            Fill in the form and we will reach back to you !
-                            Thank you for your support!
-                        </Heading>
-                        <VStack space={3} mt="5">
-                            <FormControl
-                                isRequired
-                                //isInvalid={'username' in errors}
-                            >
-                                <FormControl.Label>Name</FormControl.Label>
-                                <Input
-                                    size="lg"
-                                    type="text"
-                                    onChangeText={(value) => {
-                                        setData({ ...data, name: value });
-                                 }}
-                                />
-                                <FormControl.ErrorMessage>
-                                    {errors.name}
-                                </FormControl.ErrorMessage>
-                            </FormControl>
+                {showBox && (
+                    <Center>
+                        <Box safeArea p="2" w="90%" maxW="290" py="8">
+                            <Heading size="xl">Sponsor us today!</Heading>
+                            <Heading mt="1" size="sm" fontWeight="normal">
+                                Fill in the form and we will reach back to you !
+                                Thank you for your support!
+                            </Heading>
+                            <VStack space={3} mt="5">
+                                <FormControl
+                                    isRequired
+                                >
+                                    <FormControl.Label>Name</FormControl.Label>
+                                    <Input
+                                        size="lg"
+                                        type="text"
+                                        onChangeText={(value) => {
+                                            setData({ ...data, name: value });
+                                        }}
+                                    />
+                                </FormControl>
 
-                            <FormControl
-                                isRequired
-                                // isInvalid={'email' in errors}
-                            >
-                                <FormControl.Label>Email</FormControl.Label>
-                                <Input
-                                    size="lg"
-                                    type="text"
-                                    onChangeText={(value) => {
-                                         setData({ ...data, email: value });
-                                     }}
-                                />
-                                {/* <FormControl.ErrorMessage>
-                                    {errors.email}
-                                </FormControl.ErrorMessage> */}
-                            </FormControl>
+                                <FormControl isRequired isInvalid={!!errors.email}>
+                                    <FormControl.Label>Email</FormControl.Label>
+                                    <Input
+                                        size="lg"
+                                        type="text"
+                                        onChangeText={(value) => {
+                                            setData({ ...data, email: value });
+                                        }}
+                                    />
+                                    <FormControl.ErrorMessage>
+                                        {errors.email}
+                                    </FormControl.ErrorMessage>
+                                </FormControl>
 
+                                <Button
+                                    onPress={submit}
+                                    mt="2"
+                                    colorScheme="indigo"
+                                >
+                                    Submit
+                                </Button>
+                            </VStack>
+                        </Box>
+                    </Center>
+                )}
+                {showConfirm && (
+                    <Center>
+                        <Box safeArea p="2" w="90%" maxW="290" py="8" style={styles.box}>
+                            <Heading size="xl">Thank you!</Heading>
+                            <Heading mt="1" size="sm" fontWeight="normal">
+                                Your submission has been sent. We will reach out to you as soon as possible!
+                            </Heading>
+                            <br></br>
                             <Button
-                                onPress={submit}
+                                onPress={back}
                                 mt="2"
                                 colorScheme="indigo"
                             >
-                                Submit
+                                Return
                             </Button>
-                        </VStack>
-                    </Box>
-                </Center>
+                        </Box>
+                    </Center>
+                )}
             </KeyboardAvoidingView>
             <FooterView />
         </ScrollView>
@@ -190,5 +199,20 @@ const styles = StyleSheet.create({
     container: {
         flexGrow: 1,
         justifyContent: 'center'
+    },
+
+    box: {
+        backgroundColor: 'white',
+        borderRadius: 5,
+        shadowColor: 'black',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+        padding: 50,
+        margin: 20,
     }
 });
