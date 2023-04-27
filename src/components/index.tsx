e53cd81dbee05564904c282e25c8c0d4e4bdded8
CurtisLam <tun07683@temple.edu>
2023-04-27 17:29:24 -0400

    Revert "Removed specific extension file;"
    This reverts commit 53f951ab8eac062b35dbe686138934b652376ec1.

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,3 +2,4 @@ export { default as EventView } from './EventView';
 export { default as NewEventMenu } from './NewEventMenu';
 export { default as EditProfileMenu } from './EditProfileMenu';
 export { default as FooterView } from './FooterView';
+export { default as VideoView } from './VideoView';
