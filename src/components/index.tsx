41e62be8fdac6a9a5dbabed4d423d1af3df3a918
CurtisLam <tun07683@temple.edu>
2023-04-27 18:12:57 -0400

    Removed platform extension files and replaced with inline platform specific code.

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,4 +2,3 @@ export { default as EventView } from './EventView';
 export { default as NewEventMenu } from './NewEventMenu';
 export { default as EditProfileMenu } from './EditProfileMenu';
 export { default as FooterView } from './FooterView';
-export { default as VideoView } from './VideoView';
